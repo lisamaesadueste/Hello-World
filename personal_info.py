56a93e8cc01890062014da8a5c5b55da72c622b1
lisamaesadueste <82501107+lisamaesadueste@users.noreply.github.com>
2021-04-14 16:18:48 +0800

    Add files via upload

---
+++ personal_info.py
@@ -0,0 +1,14 @@
+print("personal information")
+
+Firstname = str(input("What is your First name? "))
+space =print(" ")
+
+Lastname = str(input("What is your Last name? "))
+
+space = print(" ")
+Location = str(input("What is your Location? "))
+space = print(" ")
+Age = str(input("What is your Age? "))
+space = " "
+
+print(Firstname+space+Lastname+space+Location+space+ Age)
